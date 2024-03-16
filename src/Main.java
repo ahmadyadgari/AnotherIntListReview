af29479d7b45399ccc1d146b39869626c9c36973
Ahmad Reshad Yadgari <ahmadreshadyadgari@gmail.com>
2024-03-15 21:10:49 -0700

    Added ArrayIntList.java class, LinkedIntList.java class and also main is compiling.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+// Ahmad Reshad Yadgari SDEV301
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        IntList list1 = new ArrayIntList();
+        IntList list2 = new LinkedIntList();
+
+
+        // add 3 ints to the back of the list
+        list1.addBack(42);
+        list1.addBack(82);
+        list1.addBack(97);
+
+        System.out.println(list1);
+
     }
 }
